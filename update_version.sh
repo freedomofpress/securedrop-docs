87d07201ce4fb2124d152f5ff001c7fd903caa5f
Erik Moeller <eloquence@gmail.com>
2020-09-22 17:57:13 -0700

    Add basic version update script

---
+++ update_version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## Usage: ./update_version.sh <version>
+
+set -e
+set -o pipefail
+
+readonly NEW_VERSION=$1
+if [ -z "$NEW_VERSION" ]; then
+  echo "You must specify the new version!"
+  exit 1
+fi
+readonly OLD_VERSION=$(grep -oP '(?<=^release \= ")\d+\.\d+\.\d+' docs/conf.py)
+
+sed -i "s@$(echo "${OLD_VERSION}" | sed 's/\./\\./g')@$NEW_VERSION@g" docs/set_up_admin_tails.rst
+sed -i "s@$(echo "${OLD_VERSION}" | sed 's/\./\\./g')@$NEW_VERSION@g" docs/conf.py
+
+echo "Versions updated. Verify the results with 'git diff' and be sure to tag"
+echo "a new stable version as part of the release process."
